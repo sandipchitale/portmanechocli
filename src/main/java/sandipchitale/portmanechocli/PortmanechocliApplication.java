80721e8b28f0a00d9aaebf425cbe45f06831aa54
SANDIP CHITALE <sandipchitale@gmail.com>
2023-11-04 05:52:35 -0700

    Use @Command on class.

--- src/main/java/sandipchitale/portmanechocli/PortmanechocliApplication.java
+++ src/main/java/sandipchitale/portmanechocli/PortmanechocliApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.command.annotation.CommandScan;
+import org.springframework.shell.command.annotation.EnableCommand;
 import org.springframework.shell.jline.PromptProvider;
-import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @SpringBootApplication
 public class PortmanechocliApplication {
@@ -21,27 +24,28 @@ public class PortmanechocliApplication {
 		}
 	}
 
-//	@Command(command = {"pe"}, description = "Access Postman Echo")
-	@ShellComponent
-	static class PostmanEchoCommand {
+	@Command()
+//	@ShellComponent
+	@EnableCommand(PostmanEchoCommands.class)
+	static class PostmanEchoCommands {
 		private final RestClient restClient;
 
-		PostmanEchoCommand(RestClient.Builder restClientBuilder) {
+		PostmanEchoCommands(RestClient.Builder restClientBuilder) {
 			this.restClient = restClientBuilder.baseUrl("https://postman-echo.com").build();
 		}
 
-//		@Command(command = "get", description = "Access Postman Echo GET")
-		@ShellMethod(key = "get", value = "Access Postman Echo /get")
+		@Command(command = "get", description = "Access Postman Echo GET")
+//		@ShellMethod(key = "get", value = "Access Postman Echo /get")
 		public String get() {
 			return restClient
 					.get()
-					.uri("/get")
+					.uri(UriComponentsBuilder.fromUriString("/get").queryParam("method", "get").build().toUriString())
 					.retrieve()
 					.body(String.class);
 		}
 
-//		@Command(command = "post", description = "Access Postman Echo POST")
-		@ShellMethod(key = "post", value = "Access Postman Echo /post")
+		@Command(command = "post", description = "Access Postman Echo POST")
+//		@ShellMethod(key = "post", value = "Access Postman Echo /post")
 		public String post() {
 			return restClient
 					.post()
@@ -52,19 +56,20 @@ public class PortmanechocliApplication {
 					.body(String.class);
 		}
 
-//		@Command(command = "put", description = "Access Postman Echo PUT")
-		@ShellMethod(key = "put", value = "Access Postman Echo /put")
+		@Command(command = "put", description = "Access Postman Echo PUT")
+//		@ShellMethod(key = "put", value = "Access Postman Echo /put")
 		public String put() {
 			return restClient
 					.put()
 					.uri("/put")
-					.contentType(MediaType.APPLICATION_FORM_URLENCODED).body("method=put")
+					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+						.body("method=put")
 					.retrieve()
 					.body(String.class);
 		}
 
-//		@Command(command = "delete", description = "Access Postman Echo DELETE")
-		@ShellMethod(key = "delete", value = "Access Postman Echo /delete")
+		@Command(command = "delete", description = "Access Postman Echo DELETE")
+//		@ShellMethod(key = "delete", value = "Access Postman Echo /delete")
 		public String delete() {
 			return restClient
 					.delete()
